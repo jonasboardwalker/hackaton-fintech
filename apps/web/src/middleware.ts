dad9b26ff032f32d39a0c927d5917602fed48247
Daniil Baznamin <jonasboardwalker@gmail.com>
2025-04-04 23:16:49 +0200

    chore: fix middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,23 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "~/i18n/routing";
 import { ROUTES } from "~/lib/routes";
-import { NextResponse } from "next/server";
 
 const intlMiddleware = createMiddleware(routing);
 
-const dashboardMatcher = createRouteMatcher([`${ROUTES.DASHBOARD}(.*)`]);
+const dashboardMatcher = createRouteMatcher([
+  `/:locale${ROUTES.DASHBOARD}(.*)`,
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { sessionClaims } = await auth();
-  const userRole = sessionClaims?.metadata?.role;
   if (dashboardMatcher(req)) {
-    // Protect access to the dashboard
-    if (userRole !== "admin" && userRole !== "user") {
-      // Redirect to the home page if no user role is found
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-
-    // Allow access to the dashboard based on role
     await auth.protect();
   }
 
