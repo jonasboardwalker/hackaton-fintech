02c17a24ed00af4130f9565583cc8043bef140a2
Tomas Necuda <tomas.necuda@necky.me>
2025-04-05 19:44:10 +0200

    Remove unused trpc file

--- apps/web/src/server/api/routers/transactions/getPublicData.ts
+++
@@ -1,8 +0,0 @@
-import { publicProcedure } from "../../trpc";
-
-export const getPublicData = publicProcedure.query(async ({ ctx }) => {
-  return {
-    info: "This is publicly accessible data. No auth required!",
-    timestamp: new Date(),
-  };
-}); 
--- apps/web/src/server/api/routers/transactions/index.ts
+++ apps/web/src/server/api/routers/transactions/index.ts
@@ -3,12 +3,10 @@
 import { createTRPCRouter } from "../../trpc";
 import { checkTransaction } from "./checkTransaction";
 import { checkTransactionFromApp } from "./checkTransactionFromApp";
-import { getPublicData } from "./getPublicData";
 import { getTransactions } from "./getTransactions";
 
 export const transactionRouter = createTRPCRouter({
   checkTransaction,
   checkTransactionFromApp,
-  getPublicData,
   getTransactions,
 });
